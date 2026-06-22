c568cb795aaaecfc63473a9cb00f2574100f2eaa
cgmmmmm <194980529+cgmmmmm@users.noreply.github.com>
2026-06-22 10:55:49 +0800

    daily: 22/6/2026

--- Easy/1189. Maximum Number of Balloons/solution.cpp
+++ Easy/1189. Maximum Number of Balloons/solution.cpp
@@ -1,20 +1,21 @@
 class Solution {
-    public int maxNumberOfBalloons(String text) {
-        int[] arr = new int[26];
-        arr[1] = 1;
-        arr[0] = 1;
-        arr[11] = 2;
-        arr[14] = 2;
-        arr[13] = 1;
+public:
+    int maxNumberOfBalloons(string text) {
+        std::vector<int> balloon(26, 0);
+        balloon[1] = 1; // b
+        balloon[0] = 1; // a
+        balloon[11] = 2; // l
+        balloon[14] = 2; // o
+        balloon[13] = 1; // n
 
-        int[] freq = new int[26];
-        for (char c : text.toCharArray()) freq[c - 'a']++;
+        std::vector<int> freq(26, 0);
+        for (char c : text) freq[c - 'a']++;
 
-        int b = arr[1];
-        int a = arr[0];
-        int l = arr[11];
-        int o = arr[14];
-        int n = arr[13];
+        int b = balloon[1];
+        int a = balloon[0];
+        int l = balloon[11];
+        int o = balloon[14];
+        int n = balloon[13];
         int res = 0;
         while (b <= freq[1] && a <= freq[0] && l <= freq[11] && o <= freq[14] && n <= freq[13])
         {
@@ -28,4 +29,4 @@ class Solution {
 
         return res;
     }
-}
+};
